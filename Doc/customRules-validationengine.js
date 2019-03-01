8480a68668f55cba749c997e173997068ee48ff3
Baki Öztürk <22.exe.94@gmail.com>
2019-03-01 09:24:52 +0300

    1.03

---
+++ Doc/customRules-validationengine.js
@@ -0,0 +1,18 @@
+﻿(function ($) {
+    $.validationEngineLanguage.allRules["ajaxEmailControl"] = {
+        "url": "/User/ValidateUserMail",
+        "extraDataDynamic": ['#Email', '#Id'],
+        "alertText": translationsValidation["ajaxEmailControl"].alertText,
+        "alertTextOk": translationsValidation["ajaxEmailControl"].alertTextOk,
+        "alertTextLoad": translationsValidation["ajaxEmailControl"].alertTextLoad
+
+    };
+    $.validationEngineLanguage.allRules["ajaxUsernameControl"] = {
+        "url": "/User/ValidateUserName",
+        "extraDataDynamic": ['#Username', '#Id'],
+        "alertText": translationsValidation["ajaxUsernameControl"].alertText,
+        "alertTextOk": translationsValidation["ajaxUsernameControl"].alertTextOk,
+        "alertTextLoad": translationsValidation["ajaxUsernameControl"].alertTextLoad
+
+    };
+})(jQuery);
